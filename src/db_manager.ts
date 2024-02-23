a41c91c88248d5e1faadc06ca09f1c9e2cb6509f
Saad Khan <saamoz2012@gmail.com>
2024-02-23 12:33:39 -0600

    change table name

--- src/db_manager.ts
+++ src/db_manager.ts
@@ -9,10 +9,10 @@ let db = new sqlite3.Database('./db/playerdata.db', (err) => {
     console.log('Connected to the database.');
   });
 
-db.run('CREATE TABLE IF NOT EXISTS langs(name text PRIMARY KEY, gold data_type DEFAULT 0, xp data_type DEFAULT 0)');
+db.run('CREATE TABLE IF NOT EXISTS players(name text PRIMARY KEY, gold data_type DEFAULT 0, xp data_type DEFAULT 0)');
 
 
 function write_data(player: typeof Player) {
     console.log(`Writing data for player ${player.name}`)
-    
+
 }
