7332eeb5c6e04ab566331af48b865c0cb2648f15
Saad Khan <saamoz2012@gmail.com>
2024-02-23 12:32:31 -0600

    Basic OOP stuff and SQLite

---
+++ src/db_manager.ts
@@ -0,0 +1,18 @@
+import { Player } from "./player" 
+const sqlite3 = require('sqlite3')
+
+
+let db = new sqlite3.Database('./db/playerdata.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the database.');
+  });
+
+db.run('CREATE TABLE IF NOT EXISTS langs(name text PRIMARY KEY, gold data_type DEFAULT 0, xp data_type DEFAULT 0)');
+
+
+function write_data(player: typeof Player) {
+    console.log(`Writing data for player ${player.name}`)
+    
+}
